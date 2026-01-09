4986d7b84c1a08c60593d2467bca01d4e8c587f4
Kipleting Edwin <kipletingedwin4@gmail.com>
2026-01-09 13:05:26 +0000

    add a navbar

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { LayoutComponent } from './layout.component';
+import { NavbarComponentModule } from '../navbar/navbar.module';
 
 @NgModule({
-  imports: [ CommonModule, FormsModule, IonicModule,],
+  imports: [ CommonModule, FormsModule, IonicModule,NavbarComponentModule],
   declarations: [LayoutComponent ],
   exports: [LayoutComponent ]
 })
