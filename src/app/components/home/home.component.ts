0257a65d00a80c8b9bda2f39a6bae4c7aacb6366
Kipleting Edwin <kipletingedwin4@gmail.com>
2026-01-11 09:40:58 +0000

    style the home section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,4 +12,29 @@ export class HomeComponent  implements OnInit {
 
   ngOnInit() {}
 
+  serviceTimes = [
+    { title: 'Sunday Worhip', time: '10:00AM'},
+    { title: 'Midweek Fellowship', time: 'Wenesday 6:30 PM'},
+    { title: 'Youth & Teens', time: 'Friday 5:00PM'},
+  ];
+
+  ministries = [
+    {
+      name: 'Children’s Ministry',
+      description: 'Nurturing young hearts with biblical truth.',
+    },
+    {
+      name: 'Youth & Teens',
+      description: 'Empowering the next generation.',
+    },
+    {
+      name: 'Worship Team',
+      description: 'Leading the church in praise.',
+    },
+    {
+      name: 'Community Outreach',
+      description: 'Serving our neighbors with compassion.',
+    },
+  ];
+
 }
