eee54043b06ae62a055fe85b0e0a00911520a688
Kipleting Edwin <kipletingedwin4@gmail.com>
2026-01-12 20:26:46 +0000

    add two more  ministries on ministries page

--- src/app/pages/ministries/ministries.component.ts
+++ src/app/pages/ministries/ministries.component.ts
@@ -14,12 +14,16 @@ export class MinistriesComponent  implements OnInit {
 
   ministries = [
     {
-      name: 'Childrens Ministry',
-      description: 'Teaching and nurturing children in the love of Christ.'
+      name: 'Children’s Ministry',
+      description: 'Teaching and nurturing children in the love of Christ.',
     },
     {
-      name: 'Childrens Ministry',
-      description: 'Teaching and nurturing children in the love of Christ.'
+      name: 'Youth & Teens',
+      description: 'Empowering young people to grow in faith and leadership.',
+    },
+    {
+      name: 'Women’s Fellowship',
+      description: 'Encouraging spiritual growth and unity among women.',
     },
   ]
 
